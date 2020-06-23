66c7e961e57618787d70efd8d13be89d13958bac
SamuelALSN <samuelgwfirst@gmail.com>
2020-06-23 18:48:01 +0000

    Best way to Implementing Fragment in React || wrapped with empty html tag

--- src/components/search.js
+++ src/components/search.js
@@ -3,18 +3,17 @@ import React, {useState} from 'react';
 
 /*
 * The search component doesn't manage the state anymore but only passes up the event to te App component after text is entered in input field */
-const Search = ({onSearch, searchTerm}) => [
+const Search = ({onSearch, searchTerm}) => (
 
-    <label key="1" htmlFor="search">
-        Search: {''}
-    </label>,
-    <input
-        key="2"
-        id="search"
-        type="text"
-        value={searchTerm}
-        onChange={onSearch}/>
-
-];
+    <>
+        <label htmlFor="search">Search: </label>
+        <input
+            id="search"
+            type="text"
+            value={searchTerm}
+            onChange={onSearch}
+        />
+    </>
+);
 
 export default Search;
