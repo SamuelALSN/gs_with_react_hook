c26143357cb423cce7fe4a9879f35b064339b07b
SamuelALSN <samuelgwfirst@gmail.com>
2020-06-23 17:27:00 +0000

    Implementing of our custom hooks use React built'in custom hooks 2 conventions

--- src/App.js
+++ src/App.js
@@ -22,19 +22,28 @@ const App = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = useState(
-        localStorage.getItem('search') || 'React' // defining the initial state of the searchTerm
-    )
+    // We are following two conventions of React's built-in hooks here
+    // First the naming convention which puts the use prefix in front of every hook name
+    //Second the returned values are returned ass aray
+    const useSemiPersistentState = () => {
+        const [searchTerm, setSearchTerm] = useState(
+            localStorage.getItem('search') || '' // defining the initial state of the searchTerm
+        )
 
-    // our sideEffect function define the localstorage
-    // first parameters: useEffect set to the localstorage the most recent search ineraction
-    // second parameters: a dependency array of variables if one variables changes the function for side-effect is called
-    // Here the side effects function is called everytime the searchTerm changes
-     // so here this hook is updated whenever the component is first mounted but also if one of it dependencies is updated
-    useEffect(() => {
-        localStorage.setItem('search', searchTerm)
-    }, [searchTerm])
+        // our sideEffect function define the localstorage
+        // first parameters: useEffect set to the localstorage the most recent search ineraction
+        // second parameters: a dependency array of variables if one variables changes the function for side-effect is called
+        // Here the side effects function is called everytime the searchTerm changes
+        // so here this hook is updated whenever the component is first mounted but also if one of it dependencies is updated
+        useEffect(() => {
+            localStorage.setItem('search', searchTerm)
+        }, [searchTerm])
 
+        // let return the values that are needed in our App Component from  our custom hooks we created
+        return [searchTerm, setSearchTerm]
+    }
+
+    const [searchTerm, setSearchTerm] = useSemiPersistentState()
     const handleSearch = event => {
         setSearchTerm(event.target.value)
     }
